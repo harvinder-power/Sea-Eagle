5c8f6ef0755a7a32978670b80ae7156e59fffc30
Harvinder Power <harvinder.power@gmail.com>
2017-06-18 12:01:52 +0100

    Added pneumonia test data

--- image_proc.py
+++ image_proc.py
@@ -12,7 +12,7 @@ example_file = glob.glob("/Users/harvinderpower/interpret/cxr1.jpg")[0]
 im = imread(example_file, as_grey=True)
 plt.imshow(im, cmap='gray')
 
-blobs_log = blob_log(im, min_sigma = 20, max_sigma=100, num_sigma=10, threshold=0.1)
+blobs_log = blob_log(im, min_sigma = 5, max_sigma=100, num_sigma=10, threshold=0.1)
 #can also define a min_sigma to get a lower end for size determinate
 blobs_log[:,2] = blobs_log[:,2]*sqrt(2)
 numrows = len(blobs_log)
@@ -27,3 +27,6 @@ for blob in blobs_log:
 end = timer()
 print (end-start), "seconds elapsed"
 plt.show()
+
+##consider changing the system to use shades of white rather than blob based system
+## also need to remove the edges - consider using ignore feature for high gradient bones and lung fields
